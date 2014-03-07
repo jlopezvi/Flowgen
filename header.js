7a1530a60c9c1d1492bcb8fc2fe448655aae73a1
jlopezvi <jjlopezvillarejo@gmail.com>
2014-03-07 14:01:53 +0100

    version 0 of website

---
+++ header.js
@@ -0,0 +1,36 @@
+document.write('\
+    <div id="banner">\
+                <br>\
+                <div id="bannerLeft">\
+                <img src="images/logo.JPG" alt="Flowgen_logo" height="120" />\
+                <br> <br> <br>\
+                <font color=#69c>Flowgen:</font> workflow-based documentation in C/C++.\
+                <br><br>\
+                </div>\
+\
+                <a id="forkme_banner" href="https://github.com/jlopezvi/Flowgen">View on GitHub</a>\
+\
+                <!--\
+                <a href="" id="bannerRight" title="Checkstyle">\
+                  <img src="images/logo.JPG" alt="Flowgen_logo" height="50" />\
+                </a>\
+                -->\
+       <div class="clear">\
+         <hr/>\
+       </div>\
+    </div>\
+\
+    <div id="breadcrumbs">\
+      <div class="xleft">\
+          <span id="publishDate">Last Published: 01 January 2014</span>\
+          &nbsp;\
+      </div>\
+      <div class="xright">\
+\
+      </div>\
+      <div class="clear">\
+         <hr/>\
+      </div>\
+    </div>\
+\
+');
