50982bbf11e5b88db3d364e8a5f774c1084f5409
jlopezvi <jjlopezvillarejo@gmail.com>
2014-08-01 15:25:28 +0200

    introduced Map of Files into README.txt; comments in makehtml.py

--- makehtml.py
+++ makehtml.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
+#### writes out HTML files. Uses .flowdbs information and .cmapx information (for hyperlinks).
+#### HMTL files point to PNG files with the diagrams
+
 import sys
 import os
 import glob
 import csv
 
 
-#### main program
+
 
 infile_str=os.path.splitext(os.path.basename(sys.argv[1]))[0]
 print (infile_str)
@@ -16,12 +19,13 @@ htmloffline_str+="""<html><head><title>docs</title></head>"""+'\n'+"""<body><hr>
  
 
        
-
+#read corresponding .flowdb file (with the same filename as the argument of the call of makehtml.py)
 reader = csv.reader(open('flowdoc/aux_files/'+infile_str+'.flowdb', "rt", encoding="utf8"), delimiter='\t')
+#loop over annotated functions / methods
 for row in reader:
   usr_key=''.join(e for e in row[0] if e.isalnum())
+  #loop over zoom levels
   for i in range(int(row[1])+1):
-    #print(range(1))
     zoomID=''
     zoom_str=''
     if i==1:
@@ -31,6 +35,7 @@ for row in reader:
       zoomID='2'
       zoom_str='  -- zoom 2'  
     htmloffline_str+="<p>"+row[2]+zoom_str+"</p>"+"""<a name="#"""+usr_key+zoomID+""""></a>"""
+    #if hyperlinks exist in the diagrams include them
     if os.path.exists('flowdoc/aux_files/'+usr_key+zoomID+'.cmapx'):
         htmloffline_str+= """<img src="aux_files/"""+usr_key+zoomID+""".png" """    
         map_str=open('flowdoc/aux_files/'+usr_key+zoomID+'.cmapx').read()
@@ -46,7 +51,7 @@ for row in reader:
 
 htmloffline_str+="""</body></html>"""
         
-
+#write string into HTML file
 writefunc = open('flowdoc/'+infile_str+'.html',"w")
 writefunc.write(htmloffline_str)
 writefunc.close()
