6803db54e503591a3b6bb9818e10a0a1dbb371f3
jlopezvi <jjlopezvillarejo@gmail.com>
2014-08-08 13:32:56 +0200

    different zoom levels are presented in tabs in the HTML files. Introduced CSS and JS files for the HTML output
    - makefile in linux copies folder htmlCSSandJS into the documentation
    project folder
    - changed makehtml.py with a better formatting of the HTML files
    - updated file map.

--- makehtml.py
+++ makehtml.py
@@ -15,41 +15,83 @@ print (infile_str)
 
 
 htmloffline_str=''
-htmloffline_str+="""<html><head><title>docs</title></head>"""+'\n'+"""<body><hr>"""
+htmloffline_str+="""<html>
+    <head>
+      <title>docs</title>
+      <script src="htmlCSSandJS/tabcontent.js" type="text/javascript"></script>
+      <link href="htmlCSSandJS/template1/tabcontent.css" rel="stylesheet" type="text/css" />
+    </head>
+    
+    <body>"""
  
-
        
 #read corresponding .flowdb file (with the same filename as the argument of the call of makehtml.py)
 reader = csv.reader(open('flowdoc/aux_files/'+infile_str+'.flowdb', "rt", encoding="utf8"), delimiter='\t')
 #loop over annotated functions / methods
 for row in reader:
   usr_key=''.join(e for e in row[0] if e.isalnum())
-  #loop over zoom levels
-  for i in range(int(row[1])+1):
-    zoomID=''
-    zoom_str=''
-    if i==1:
-      zoomID='1'
-      zoom_str='  -- zoom 1'
-    if i==2:
-      zoomID='2'
-      zoom_str='  -- zoom 2'  
-    htmloffline_str+="<p>"+row[2]+zoom_str+"</p>"+"""<a name="#"""+usr_key+zoomID+""""></a>"""
-    #if hyperlinks exist in the diagrams include them
-    if os.path.exists('flowdoc/aux_files/'+usr_key+zoomID+'.cmapx'):
-        htmloffline_str+= """<img src="aux_files/"""+usr_key+zoomID+""".png" """    
-        map_str=open('flowdoc/aux_files/'+usr_key+zoomID+'.cmapx').read()
-        htmloffline_str+=""" USEMAP="#"""+usr_key+zoomID+'_map'
-        htmloffline_str+=""""/> """   
-        htmloffline_str+=map_str+"""<hr>
-        """
-    else:
-        htmloffline_str+= """<img src="aux_files/"""+usr_key+zoomID+'.png'    
-        htmloffline_str+=""" "> 
-        <hr>"""
-  htmloffline_str+="<hr><hr>"    
+  
+  htmloffline_str+="""
+    <hr><hr><hr>"""
+  htmloffline_str+="<p>"+row[2]+"</p>"+"""<a name="#"""+usr_key+""""></a>"""
+  #if zoom level is 0
+  if int(row[1])==0:
+      zoomID=''  
+      #if hyperlinks exist in the diagrams include them
+      if os.path.exists('flowdoc/aux_files/'+usr_key+zoomID+'.cmapx'):
+          htmloffline_str+= """
+          <img src="aux_files/"""+usr_key+zoomID+""".png" """    
+          map_str=open('flowdoc/aux_files/'+usr_key+zoomID+'.cmapx').read()
+          htmloffline_str+=""" USEMAP="#"""+usr_key+zoomID+'_map'
+          htmloffline_str+=""""/> """   
+          htmloffline_str+=map_str+'\n'
+      else:
+          htmloffline_str+= """
+          <img src="aux_files/"""+usr_key+zoomID+'.png'    
+          htmloffline_str+=""" "> """
+  #else, loop over zoom levels and put them into tabs
+  else:
+    htmloffline_str+="""
+      <ul class="tabs" data-persist="true">"""
+    for i in range(int(row[1])+1):
+      zoomID=''
+      if i==1:
+        zoomID='1'
+      if i==2:
+        zoomID='2'
+      htmloffline_str+="""
+        <li><a href="#view"""+zoomID+""" ">zoom"""+zoomID+"""</a></li>"""
+    htmloffline_str+="""
+      </ul>"""
+    
+    htmloffline_str+= """
+      <div class="tabcontents"> """
+    for i in range(int(row[1])+1):
+      zoomID=''
+      if i==1:
+        zoomID='1'
+      if i==2:
+        zoomID='2'
+      htmloffline_str+="""
+        <div id="view"""+zoomID+""" ">"""
+      #if hyperlinks exist in the diagrams include them
+      if os.path.exists('flowdoc/aux_files/'+usr_key+zoomID+'.cmapx'):
+          htmloffline_str+= """<img src="aux_files/"""+usr_key+zoomID+""".png" """    
+          map_str=open('flowdoc/aux_files/'+usr_key+zoomID+'.cmapx').read()
+          htmloffline_str+=""" USEMAP="#"""+usr_key+zoomID+'_map'
+          htmloffline_str+=""""/> """   
+          htmloffline_str+=map_str+'\n'
+      else:
+          htmloffline_str+= """<img src="aux_files/"""+usr_key+zoomID+'.png'    
+          htmloffline_str+=""" "> """
+      htmloffline_str+="</div>"
+    htmloffline_str+="""
+      </div>"""
+    
 
-htmloffline_str+="""</body></html>"""
+htmloffline_str+="""
+    </body>
+</html>"""
         
 #write string into HTML file
 writefunc = open('flowdoc/'+infile_str+'.html',"w")
