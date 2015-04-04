637adb2a6c1b4295cc7e5067648d3fe506f711cc
jlopezvi <jjlopezvillarejo@gmail.com>
2015-04-04 19:17:49 +0200

    - Solved bug in FOR loop condition.

--- makeflows.py
+++ makeflows.py
@@ -778,10 +778,13 @@ def process_find_functions(node, MAX_diagram_zoomlevel):
                         string_tmp[write_zoomlevel] += '\n' + indentation_level * tab + 'repeat''\n'
                     # 209: A for statement.
                     elif looptypeArray[IdxLoopbeginlineArray] == 209:
-                        string_condition = ' '.join(
-                            t.spelling.decode("utf-8") for t in list(node.get_children())[0].get_tokens())[:-1]+' '+' '.join(
+                        #the '0','1','2' children of the node contain the spellings of the three elements of the FOR loop. 
+                        #We have to call the command get_tokens, which produces an iterator over all tokens and then join them into the same string.
+                        #However, for the '0' and '2' children, we don't want the last token. We have first to convert the iterator into a list and then use [:-1]
+                        string_condition = 'FOR ('+' '.join(
+                            t.spelling.decode("utf-8") for t in list(list(node.get_children())[0].get_tokens())[:-1])+' '+' '.join(
                             t.spelling.decode("utf-8") for t in list(node.get_children())[1].get_tokens())+' '+' '.join(
-                            t.spelling.decode("utf-8") for t in list(node.get_children())[2].get_tokens())[:-1]
+                            t.spelling.decode("utf-8") for t in list(list(node.get_children())[2].get_tokens())[:-1])+' )'
                         string_tmp[
                             write_zoomlevel] += '\n' + indentation_level * tab + 'while (' + string_condition + ')''\n'
                 # mark } endloop to be written in string
