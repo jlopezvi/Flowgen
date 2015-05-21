a12a2e5d234cfb3c5fb92f46827afa2016a3ff31
jlopezvi <jjlopezvillarejo@gmail.com>
2015-05-21 15:45:31 +0200

    included images with hyperlinks in blue, underlined

--- header.js
+++ header.js
@@ -20,7 +20,7 @@ document.write('\
 \
     <div id="breadcrumbs">\
       <div class="xleft">\
-          <span id="publishDate">Last Published: 17 May 2015</span>\
+          <span id="publishDate">Last Published: 21 May 2015</span>\
           &nbsp;\
       </div>\
       <div class="xright">\
